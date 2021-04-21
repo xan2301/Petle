038b6bb82d99cfaf44c43c95a83ed2a5f9abb053
Przemyslaw Wiktorowski <2silverghost2@gmail.com>
2021-04-21 15:03:54 +0200

    petla

---
+++ src/com/company/LoopExamples.java
@@ -0,0 +1,26 @@
+package com.company;
+
+public class LoopExamples {
+
+    public static void main(String[] args) {
+        LoopExamples loopExamples = new LoopExamples();
+        loopExamples.whileLoopExample1();
+
+    }
+
+    private void whileLoopExample1() {
+        int i = 0;
+
+        while (i < 10) {
+
+            System.out.println("i = " + i);
+            i++;
+        }
+    }
+
+
+}
+
+
+
+
